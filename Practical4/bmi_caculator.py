c774698d461a0408bfeeee7e4f2b6105c532d95b
Maple <fenglan.24@intl.zju.edu.cn>
2025-03-14 11:38:03 +0800

    Initialize

--- Practical4/bmi_caculator.py
+++ Practical4/bmi_caculator.py
@@ -1,14 +1,14 @@
 #don't need pseudocode
 wt=input("weight:")
 ht=input("height:")
-bmi=wt/(ht*ht)
-print("BMI:",bmi)
-if bmi<18.5:
+bmi=wt/(ht*ht)          #the formula for bmi
+print("BMI:",bmi)      
+if bmi<18.5:            #less than 18.5
     print("Underweight")
-elif bmi>=18.5 and bmi<30:
+elif bmi>=18.5 and bmi<30:  #the second situation
     print("Normal weight")
 elif bmi>=30:
-    print("Obese")
+    print("Obese")    #the third situation
 
 
 
--- Practical4/triangle.py
+++ Practical4/triangle.py
@@ -1,6 +1,9 @@
 #don't need pseudocode
 n=10
 ans=0
-for i  in range(1,n+1):
-    ans+=i
-    print(ans)
+for i  in range(1,n+1): #from 1 to 10
+    ans+=i              #add i to ans
+    print(ans)          #in the for loop, print ans
+
+
+
