dda41606c7e141f92e64bc92fffe17c77fd36655
bls555 <2638130049@qq.com>
2025-04-01 18:01:28 +0800

    Update triangle.py

--- Practical4/triangle.py
+++ Practical4/triangle.py
@@ -1,4 +1,8 @@
-#don't need pseudocode
+#pseudocode:
+# initiate n and ans
+# using for  loop, from 1 to n (n+1 is not included due to the grammar
+# every time answer add an i
+# in the for loop, print ans, for a sum of 10 times
 n=10
 ans=0
 for i  in range(1,n+1): #from 1 to 10
