21b76f0f1019f8d48b1248bcd00c961c221efaca
bls555 <2638130049@qq.com>
2025-04-01 17:59:05 +0800

    Update variebles.py

--- Practical4/variebles.py
+++ Practical4/variebles.py
@@ -7,7 +7,7 @@ f=d+e#95
 print(c>e)#if print True c is longer
 #I see "compare c to e in the practical, but I think there was something wrong with that, c and e dont indicate different methods
 #compare c to f
-print(c<f)#print True, walk+bus is shorter than car
+print(c<f)#print True, walk+bus is shorter than car, which means it's the shortest.
 X=True
 Y=False
 W=X and Y#W=False
